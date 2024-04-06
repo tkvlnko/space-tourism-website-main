7a9c0e91b090a64f571d17d1657ada1e0a75fd02
Taya <tayaibragimova@gmail.com>
2024-04-06 23:27:04 +0300

    start media queries

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <footer style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', backgroundColor: '#ff2323'}}>
+    <footer style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', backgroundColor: '#0d0f1b', padding: '3rem', color: '#fff  f1d'}}>
         <p>&copy; Made by Taya Kovalenko</p>
         <p>Design: Frontend Mentor</p>
     </footer>
